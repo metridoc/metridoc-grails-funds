93cceb189d1e4359f0de6f83285ae911c3312469
tbarker <mingus.karate@gmail.com>
2014-02-06 13:51:02 -0500

    migate to github
    also uses central dataSource

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,26 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+        mavenCentral()
+    }
+    dependencies {
+        runtime 'mysql:mysql-connector-java:5.1.13'
+		compile 'org.apache.poi:poi-ooxml:3.8-beta4'
+    }
+
+    plugins {
+        build ":tomcat:7.0.47"
+    }
+}
---
+++ grails-app/views/index.gsp
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Welcome to Grails</title>       
+    </head>
+    <body>
+       <div>
+                <h2>Available Controllers:</h2>
+                <ul>
+                    <g:each var="c" in="${grailsApplication.controllerClasses.sort { it.fullName } }">
+                        <li class="controller"><g:link controller="${c.logicalPropertyName}">${c.fullName}</g:link></li>
+                    </g:each>
+                </ul>
+        </div>
+    </body>
+</html>
